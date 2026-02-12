58fd1abd3d217014237a13f78c109abfac3f5a75
Jan Pipek <jan.pipek@aisle.com>
2026-02-12 09:53:08 +0100

    Semi-initial

---
+++ src/clippt/__init__.py
@@ -0,0 +1,13 @@
+"""Create a textual"""
+
+from clippt.app import PresentationApp
+from clippt.slides import load, md, py, sh, slide
+
+__all__ = [
+    "PresentationApp",
+    "md",
+    "sh",
+    "py",
+    "load",
+    "slide",
+]
