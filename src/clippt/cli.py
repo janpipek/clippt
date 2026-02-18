09d5334f8ac364386077f7f97fc7cb0c696a563a
Jan Pipek <jan.pipek@gmail.com>
2026-02-18 08:21:06 +0100

    Enable loading dictionaries

--- src/clippt/cli.py
+++ src/clippt/cli.py
@@ -1,13 +1,9 @@
-import tomllib
-from itertools import chain
 from pathlib import Path
-from typing import Any
 
 import click
-from typing_extensions import Iterable
 
 from clippt.app import PresentationApp
-from clippt.slides import MarkdownSlide, PythonSlide, ShellSlide, Slide, load
+from clippt.slides import Slide, load
 from clippt.presentation import load_presentation
 
 
@@ -17,7 +13,7 @@ from clippt.presentation import load_presentation
     type=click.Path(
         exists=True,
         file_okay=True,
-        dir_okay=False,
+        dir_okay=True,
         path_type=Path,
     ),
 )
@@ -26,6 +22,8 @@ from clippt.presentation import load_presentation
     "--continue", "-c", "continue_", is_flag=True, help="Continue from last slide."
 )
 def clippt(source: Path, disable_footer: bool, continue_: bool):
+    """Run a presentation in the command-line."""
+
     slides, title = load_slides(source)
     app = PresentationApp(slides=slides, title=title)
     app.enable_footer = not disable_footer
@@ -36,40 +34,11 @@ def clippt(source: Path, disable_footer: bool, continue_: bool):
 
 
 def load_slides(source: Path) -> tuple[list[Slide], str]:
-    def _load_single(item: Any) -> Iterable[Slide]:
-        if isinstance(item, str):
-            yield load(source.parent / item)
-        if isinstance(item, dict):
-            content = item.get("content", "")
-            path = (
-                source.parent / Path(path_str)
-                if (path_str := item.get("path"))
-                else None
-            )
-            if not content and path:
-                content = path.read_text(encoding="utf-8")
-            title = item.get("title", None)
-            alt_screen = item.get("alt_screen", False)
-            mode = item.get("mode", "code")
-            match item.get("type", "markdown"):
-                case "markdown":
-                    if title:
-                        content = f"# {title}\n\n{content}"
-                    yield MarkdownSlide(content, path=path)
-                case "python":
-                    yield PythonSlide(
-                        source=content,
-                        title=title,
-                        alt_screen=alt_screen,
-                        mode=mode,
-                        path=path,
-                    )
-                case "shell":
-                    yield ShellSlide(
-                        source=content, title=title, alt_screen=alt_screen, mode=mode
-                    )
-                case _:
-                    raise ValueError(f"Unsupported slide type: {item['type']}")
+    if source.is_dir():
+        if (source / "presentation.toml").exists():
+            source = source / "presentation.toml"
+        else:
+            raise FileNotFoundError(f"presentation.toml not found in {source}")
 
     match source.suffix.lower():
         case ".md" | ".markdown" | ".csv" | ".py" | ".pq" | ".parquet":
