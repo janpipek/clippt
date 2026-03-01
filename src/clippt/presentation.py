f25947d96c23e18e11ccaeea1f5e82267260ca55
Jan Pipek <jan.pipek@aisle.com>
2026-03-01 22:44:28 +0100

    Fix: wrong load path for slides

--- src/clippt/presentation.py
+++ src/clippt/presentation.py
@@ -128,7 +128,7 @@ def load_presentation(path_or_file: Path | str | io.TextIOBase, /) -> Presentati
         content = path_or_file.read()
         pwd = Path(".")
         data = tomllib.loads(content)
-        return Presentation.model_validate(data | {"pwd": pwd})
+        presentation = Presentation.model_validate(data | {"slide_base_path": pwd})
     else:
         path = Path(path_or_file)
         pwd = path.parent
@@ -136,9 +136,10 @@ def load_presentation(path_or_file: Path | str | io.TextIOBase, /) -> Presentati
         match path.suffix.lower():
             case ".toml":
                 data = tomllib.loads(path.read_text())
-                return Presentation.model_validate(data | {"pwd": pwd})
+                presentation = Presentation.model_validate(data | {"slide_base_path": pwd})
             case ".json":
                 data = json.loads(path.read_text())
-                return Presentation.model_validate(data | {"pwd": pwd})
+                presentation = Presentation.model_validate(data | {"slide_base_path": pwd})
             case _:
                 raise ValueError(f"Cannot parse {path}")
+    return presentation
